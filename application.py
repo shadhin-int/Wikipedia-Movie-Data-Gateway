d3bd1293430b95f9ba32622ad3e8b642cfe17206
Shahinur Rahman <shadhin.int@outlook.com>
2020-02-28 11:22:43 +0600

    Stored movie data in sqlite database from multiple source and stored

--- application.py
+++ application.py
@@ -3,13 +3,17 @@
 import sys
 import requests
 from bs4 import BeautifulSoup
+from flask import Flask, jsonify
+import sqlite3
 
 home_html_page = "https://en.wikipedia.org/wiki/List_of_Academy_Award-winning_films"
 base_url = "https://en.wikipedia.org"
 movie_lists = []
+SQLITE_DATABASE = "movieinfo.db"
 
 
 def scrape_data_from_wikipedia():
+    init_database()
 
     get_request = requests.get(home_html_page)
     parsed_html = BeautifulSoup(get_request.content, features="html.parser")
@@ -60,8 +64,84 @@ def scrape_data_from_wikipedia():
                 'film_inner_info': single_movie_inner_info
             }
 
+            movie_info_store_in_db(single_movie_complete_info)
+
         except Exception as ex:
             pass
         # break
 
     print("Successfully scrape data from wikipedia")
+
+
+def init_database():
+    db_conn = sqlite3.connect(SQLITE_DATABASE)
+    init_cursor = db_conn.cursor()
+    init_cursor.execute('''DROP TABLE IF EXISTS movies;''')
+    init_cursor.execute('''DROP TABLE IF EXISTS movie_info;''')
+
+    init_cursor.execute('''
+    CREATE TABLE IF NOT EXISTS movies (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    name TEXT NOT NULL,
+    year TEXT NOT NULL,
+    link TEXT NOT NULL,
+    full_link TEXT NOT NULL,
+    awards TEXT NOT NULL,
+    nominations TEXT NOT NULL
+    );
+    ''')
+
+    init_cursor.execute('''
+    CREATE TABLE IF NOT EXISTS movie_info (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    name TEXT NOT NULL,
+    value TEXT NOT NULL,
+
+    movie_id INTEGER NOT NULL,
+    FOREIGN KEY (movie_id) REFERENCES movies (id)
+    );
+    ''')
+    db_conn.commit()
+    db_conn.close()
+    return "Successfully Database Initialization"
+
+
+def movie_info_store_in_db(single_movie_complete_info):
+    print(single_movie_complete_info)
+    # print(single_movie_complete_info['film_info'])
+    # print(single_movie_complete_info['film_inner_info'])
+
+    db_conn = sqlite3.connect(SQLITE_DATABASE)
+    movie_cursor = db_conn.cursor()
+    movie_cursor.execute('INSERT INTO movies (name, year, link, full_link, awards, nominations) VALUES (?,?,?,?,?,?)', [
+        single_movie_complete_info['film_info']['name'],
+        single_movie_complete_info['film_info']['year'],
+        single_movie_complete_info['film_info']['link'],
+        single_movie_complete_info['film_info']['full_link'],
+        single_movie_complete_info['film_info']['awards'],
+        single_movie_complete_info['film_info']['nominations'],
+    ])
+
+    for info in single_movie_complete_info['film_inner_info']:
+        cur = db_conn.cursor()
+        cur.execute('INSERT INTO movie_info (name, value, movie_id) VALUES (?,?,?)', [
+            info['name'],
+            info['value'],
+            movie_cursor.lastrowid
+        ])
+
+    db_conn.commit()
+    db_conn.close()
+
+
+def main():
+    for arg in sys.argv[1:]:
+        if arg == "parse":
+            print("Scraping Server Running....")
+            scrape_data_from_wikipedia()
+        else:
+            print("Please pass-running mode")
+
+
+if __name__ == "__main__":
+    main()
